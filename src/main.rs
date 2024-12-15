2ab558817b7aa172d0668158fcec1d16288d8e1a
chhaawske <chhaawske7@gmail.com>
2024-12-15 11:46:42 +0100

    counter

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,44 @@
 use std::io;
 
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
-    crossterm::event::{self, KeyCode, KeyEventKind},
+    buffer::Buffer,
+    layout::Rect,
     style::Stylize,
-    widgets::Paragraph,
-    DefaultTerminal,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+    DefaultTerminal, Frame,
 };
 
 fn main() -> io::Result<()> {
     let mut terminal = ratatui::init();
-    terminal.clear()?;
-    let app_result = run(terminal);
+    let app_result = App::default().run(&mut terminal);
     ratatui::restore();
     app_result
 }
 
-fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
-    loop {
-        terminal.draw(|frame| {
-            let greeting = Paragraph::new("Hello Ratatui! (press 'q' to quit)")
-                .white()
-                .on_blue();
-            frame.render_widget(greeting, frame.area());
-        })?;
+#[derive(Debug, Default)]
+pub struct App {
+    counter: u8,
+    exit: bool,
+}
 
-        if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                return Ok(());
-            }
+impl App {
+
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
         }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        todo!()
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        todo!()
     }
 }
